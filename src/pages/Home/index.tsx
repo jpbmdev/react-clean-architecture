9c5db84d04203ece4e924e58338fb8757677399e
jpbmdev <jpbmdev@gmail.com>
2023-04-16 11:21:53 -0500

    based grid finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,50 @@
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useCallback, useState } from "react";
+
+import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
 import { People } from "../../data";
+import { Person } from "../../models";
+import { Checkbox } from "@mui/material";
 
 export interface HomeInterface {}
 
 export const Home: React.FC<HomeInterface> = () => {
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
+
+  const findPerson = useCallback(
+    (person: Person) => !!selectedPeople.find((p) => p.id === person.id),
+    [selectedPeople]
+  );
+
+  const filterPerson = useCallback(
+    (person: Person) => selectedPeople.filter((p) => p.id !== person.id),
+    [selectedPeople]
+  );
+
+  const handleChange = useCallback(
+    (person: Person) => {
+      setSelectedPeople(
+        findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
+      );
+    },
+    [selectedPeople]
+  );
+
   const columns: GridColDef[] = [
+    {
+      field: "actions",
+      type: "actions",
+      sortable: false,
+      headerName: "",
+      flex: 1,
+      maxWidth: 50,
+      renderCell: (params: GridCellParams) => (
+        <Checkbox
+          size="small"
+          checked={findPerson(params.row)}
+          onChange={() => handleChange(params.row)}
+        />
+      ),
+    },
     {
       field: "name",
       headerName: "Name",
